80e6bdcfd346fb0a23635925e4b9e92dc0833e9c
Cho-SangHyun <98shcho@naver.com>
2025-12-15 23:56:12 +0900

    first commit

---
+++ src/main/java/rays/techlab/fde/FdeApplication.java
@@ -0,0 +1,13 @@
+package rays.techlab.fde;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FdeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FdeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/rays/techlab/fde/FdeApplicationTests.java
@@ -0,0 +1,13 @@
+package rays.techlab.fde;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FdeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
