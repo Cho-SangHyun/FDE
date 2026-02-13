6102fce2f590fc1edff2333bde0626741a497606
Cho-SangHyun <98shcho@naver.com>
2026-02-14 00:17:47 +0900

    refactor: 전문 직렬화, 역직렬화에 자체 개발한 라이브러리 사용

--- src/main/java/rays/techlab/fde/job/extract/dto/AccountInformationResultItem.java
+++ src/main/java/rays/techlab/fde/job/extract/dto/AccountInformationResultItem.java
@@ -1,15 +1,26 @@
 package rays.techlab.fde.job.extract.dto;
 
+import io.github.Cho_SangHyun.fixedbyte.annotation.Align;
+import io.github.Cho_SangHyun.fixedbyte.annotation.FixedByteField;
+import io.github.Cho_SangHyun.fixedbyte.annotation.FixedByteRecord;
+
 import java.math.BigDecimal;
 
+@FixedByteRecord(encoding = "EUC-KR")
 public record AccountInformationResultItem(
-
+        @FixedByteField(order = 1, length = 8, align = Align.RIGHT, padChar = '0')
         String sequenceNumber,
+        @FixedByteField(order = 2, length = 30, align = Align.LEFT)
         String custName,
+        @FixedByteField(order = 3, length = 14, align = Align.LEFT)
         String inhabitantNumber,
+        @FixedByteField(order = 4, length = 30, align = Align.LEFT)
         String accountNumber,
+        @FixedByteField(order = 6, length = 20, align = Align.LEFT)
         String productName,
+        @FixedByteField(order = 5, length = 2, align = Align.LEFT)
         String productType,
+        @FixedByteField(order = 7, length = 20, align = Align.LEFT)
         BigDecimal balance
 ) {
 }
